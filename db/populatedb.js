73fdf79f50343fb71262b45be3826e203abea3b6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-17 13:28:34 +0000

    feat: add script to populate the db with tables

---
+++ db/populatedb.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const SQL = `
+
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    member_status BOOLEAN NOT NULL DEFAULT FALSE
+  );
+
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    text TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  );
+
+  CREATE TABLE IF NOT EXISTS user_sessions (
+    sid VARCHAR(255) PRIMARY KEY,
+    sess JSON NOT NULL,
+    expire TIMESTAMPTZ NOT NULL
+  );
+
+
+`;
+
+const main = async () => {
+  const env = process.argv[2] || 'dev';
+  const dbURL =
+    env === 'dev'
+    ? process.env.DEV_DATABASE_URL
+    : process.env.PROD_DATABASE_URL
+
+  console.log('Seeding...');
+
+  const client = new Client({
+    connectionString: dbURL
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+
+  console.log('Finished seeding.');
+};
+
+main();

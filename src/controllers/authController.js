655067cefd5630f025456603eb09b2e8291a67f6
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-17 16:30:16 +0000

    feat: add a post register account with hashed password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,16 @@
-const register = (req, res) => {
+const bcrypt = require('bcryptjs');
+const db = require('../../db/queries');
+
+const getRegister = (req, res) => {
   res.render('register', { title: 'Register Account' });
 }
 
-module.exports = { register };
+const postRegister = async (req, res) => {
+  const { first_name, last_name, username, email, password, confirmPassword } = req.body;
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await db.createUser(first_name, last_name, username, email, hashedPassword);
+  res.redirect('/'); // '/auth/login' when route created or account created confirmation page
+}
+
+module.exports = { getRegister, postRegister };

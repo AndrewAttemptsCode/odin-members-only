faf6986e8d4ac2d2e95f4673424cf5ec8cfdd1a2
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-18 15:54:13 +0000

    feat: add register middleware form validation to post route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
+const { registerValidation } = require('../validators/registerValidation');
 
 const authRoute = Router();
 
 authRoute.get('/register', authController.getRegister);
-authRoute.post('/register', authController.postRegister);
+authRoute.post('/register', registerValidation, authController.postRegister);
 
 module.exports = authRoute;

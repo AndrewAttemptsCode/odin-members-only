9821f459580583b041389564bd7ba85f79c23362
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-19 16:28:39 +0000

    feat: add get and post routes for login and authentication with passport local strategy

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
 const { registerValidation } = require('../validators/registerValidation');
+const passport = require('passport');
 
 const authRoute = Router();
 
 authRoute.get('/register', authController.getRegister);
 authRoute.post('/register', registerValidation, authController.postRegister);
+
+authRoute.get('/login', authController.getLogin);
+authRoute.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth/login'
+}))
 
 module.exports = authRoute;

b9b22c8a4a8ce45c650711e6816f32837c704731
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-22 16:01:08 +0000

    feat: add message validation middleware to create message route

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const indexController = require('../controllers/indexController');
 const isAuthAndMember = require('../validators/isAuthAndMember');
+const { messageValidation } = require('../validators/messageValidation');
 
 const indexRoute = Router();
 
 indexRoute.get('/', indexController.getIndex);
 indexRoute.get('/create-message', isAuthAndMember, indexController.getCreateMessage);
-indexRoute.post('/create-message', isAuthAndMember, indexController.postCreateMessage);
+indexRoute.post('/create-message', isAuthAndMember, messageValidation, indexController.postCreateMessage);
 
 module.exports = indexRoute;

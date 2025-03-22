c7cf215c6b3a5f0efb2236e6de751c1ae3a4ee34
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-22 14:29:18 +0000

    feat: add post logic for new message

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,3 +1,6 @@
+const db = require('../../db/queries');
+const asyncHandler = require('express-async-handler');
+
 const getIndex = (req, res) => {
   res.render('index', { title: 'Members Club', user: req.user });
 }
@@ -6,4 +9,12 @@ const getCreateMessage = (req, res) => {
   res.render('newmessage', { title: 'Create New Message' });
 }
 
-module.exports = { getIndex, getCreateMessage };
+const postCreateMessage = asyncHandler(async (req, res) => {
+  const { title, text } = req.body;
+  const userID = req.user.id;
+
+  await db.createMessage(userID, title, text);
+  res.redirect('/');
+})
+
+module.exports = { getIndex, getCreateMessage, postCreateMessage };

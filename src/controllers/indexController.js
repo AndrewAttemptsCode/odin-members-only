cbe4c15e1e503609e7079a399e13c9ecd97bd86f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-22 16:00:27 +0000

    feat: add form error handling

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,6 @@
 const db = require('../../db/queries');
 const asyncHandler = require('express-async-handler');
+const { validationResult } = require('express-validator');
 
 const getIndex = (req, res) => {
   res.render('index', { title: 'Members Club', user: req.user });
@@ -10,10 +11,20 @@ const getCreateMessage = (req, res) => {
 }
 
 const postCreateMessage = asyncHandler(async (req, res) => {
-  const { title, text } = req.body;
+  const errors = validationResult(req);
+  const { messageTitle, messageText } = req.body;
   const userID = req.user.id;
 
-  await db.createMessage(userID, title, text);
+  if (!errors.isEmpty()) {
+    return res.status(400).render('newmessage', {
+      title: 'Create New Message',
+      errors: errors.array(),
+      messageTitle: messageTitle,
+      messageText: messageText
+    });
+  }
+
+  await db.createMessage(userID, messageTitle, messageText);
   res.redirect('/');
 })
 

344f76894b901dfecccd088b64f0662bcdc1ebe8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-24 15:27:40 +0000

    feat: add dynamic route to deleted a message based on message id and if the user is logged in and has admin rights

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,6 +3,7 @@ const authController = require('../controllers/authController');
 const { registerValidation } = require('../validators/registerValidation');
 const passport = require('passport');
 const isAuth = require('../validators/isAuth');
+const isAdmin = require('../validators/isAdmin');
 
 const authRoute = Router();
 
@@ -19,6 +20,8 @@ authRoute.get('/join-club', isAuth, authController.getJoinClub);
 authRoute.post('/join-club', authController.postJoinClub);
 
 authRoute.get('/logout', isAuth, authController.logout);
+
+authRoute.get('/delete-message/:messageID', isAuth, isAdmin, authController.deleteMessage);
 
 module.exports = authRoute;
 

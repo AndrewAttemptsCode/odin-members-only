f72af4c5435c08b03826a042c9b75c88565ab537
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-28 18:41:10 +0000

    feat: add restrictions to the db pool

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  connectionString: process.env.PROD_DATABASE_URL
+  connectionString: process.env.PROD_DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 })
